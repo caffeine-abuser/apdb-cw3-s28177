cec5950c25696804045e0f51422c545debea1ccd
caffeine-abuser <s28177@pjwstk.edu.pl>
2026-05-06 23:04:12 +0200

    add dtos and query string filter objects

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -5,14 +5,32 @@ public enum ReservationStatus
     CANCELLED
 }
 
-public class Reservation(int id, int roomId, string organizerName, string topic, DateOnly date, DateTime startTime, DateTime endTime, ReservationStatus status)
+public class Reservation(Guid? id, ReservationDTO data)
 {
-    int Id = id;
-    int RoomId = roomId;
-    string OrganizerName = organizerName;
-    string Topic = topic;
-    DateOnly Date = date;
-    DateTime StartTime = startTime;
-    DateTime EndTime = endTime;
-    ReservationStatus Status = status;
+    public Guid Id = id ?? new Guid();
+    public Guid RoomId = data.RoomId;
+    public string OrganizerName = data.OrganizerName;
+    public string Topic = data.Topic;
+    public DateOnly Date = data.Date;
+    public DateTime StartTime = data.StartTime;
+    public DateTime EndTime = data.EndTime;
+    public ReservationStatus Status = data.Status;
+}
+
+public class ReservationDTO(Guid roomId, string organizerName, string topic, DateOnly date, DateTime startTime, DateTime endTime, ReservationStatus status)
+{
+    public Guid RoomId = roomId;
+    public string OrganizerName = organizerName;
+    public string Topic = topic;
+    public DateOnly Date = date;
+    public DateTime StartTime = startTime;
+    public DateTime EndTime = endTime;
+    public ReservationStatus Status = status;
+}
+
+public class ReservationFilterDTO(DateOnly date, ReservationStatus status, Guid roomId)
+{
+    public DateOnly date = date;
+    public ReservationStatus status = status;
+    public Guid RoomId = roomId;
 }
--- Models/Room.cs
+++ Models/Room.cs
@@ -1,10 +1,36 @@
-public class Room(int id, string name, string buildingCode, int floor, int capacity, bool hasProjector, bool isActive)
+using System.ComponentModel.DataAnnotations;
+
+public class Room(Guid? id, RoomDTO room)
 {
-    int Id = id;
-    string Name = name;
-    string BuildingCode = buildingCode;
-    int Floor = floor;
-    int Capacity = capacity;
-    bool HasProjector = hasProjector;
-    bool IsActive = isActive;
+    [Required]
+    public Guid Id = id ?? new Guid();
+    [Required]
+    public string Name = room.Name;
+    [Required]
+    public string BuildingCode = room.BuildingCode;
+    public int Floor = room.Floor;
+    [Range(1, int.MaxValue)]
+    public int Capacity = room.Capacity;
+    public bool HasProjector = room.HasProjector;
+    public bool IsActive = room.IsActive;
+}
+
+public class RoomDTO(string name, string buildingCode, int floor, int capacity, bool hasProjector, bool isActive)
+{
+    [Required]
+    public string Name = name;
+    [Required]
+    public string BuildingCode = buildingCode;
+    public int Floor = floor;
+    [Range(1, int.MaxValue)]
+    public int Capacity = capacity;
+    public bool HasProjector = hasProjector;
+    public bool IsActive = isActive;
+}
+
+public class RoomFilterDTO(int? minCapacity, bool? hasProjector, bool? isActive)
+{
+    public int? MinCapacity = minCapacity;
+    public bool? HasProjector = hasProjector;
+    public bool? IsActive = isActive;
 }
