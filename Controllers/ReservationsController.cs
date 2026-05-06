3ade0765cc34a150704d19106afe72c9ed063257
caffeine-abuser <s28177@pjwstk.edu.pl>
2026-05-06 23:04:00 +0200

    scaffold reservations controlelr

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -6,5 +6,49 @@ namespace apdb_cw3_s28177.Controllers;
 [Route("api/[controller]")]
 public class ReservationsController : ControllerBase
 {
-    
+    private readonly ILogger<ReservationsController> _logger;
+    public List<Reservation> storedReservations = [
+        // TODO: populate with 4-6 test reservations
+    ];
+
+    public ReservationsController(ILogger<ReservationsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllReservations()
+    {
+        
+    }
+
+    [HttpGet]
+    public IActionResult GetReservationsByFilter([FromQuery] ReservationFilterDTO data)
+    {
+        
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetReservationById(Guid id)
+    {
+        
+    }
+
+    [HttpPost]
+    public IActionResult CreateReservation(ReservationDTO data)
+    {
+        
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateReservation(Guid id, ReservationDTO data)
+    {
+        
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteReservation(Guid id)
+    {
+        
+    }
 }
