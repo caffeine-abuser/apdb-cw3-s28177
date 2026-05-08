a79f847af98287b1fecb7e2d8b9f0e05c5c7a80e
caffeine-abuser <s28177@pjwstk.edu.pl>
2026-05-08 23:02:20 +0200

    make enums usable w/ strings

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Source - https://stackoverflow.com/a/59096114
+// Posted by Andrei, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-05-08, License - CC BY-SA 4.0
+// allows API consumers to use string enum values instead of raw ints
+builder.Services.AddMvc().AddJsonOptions(opts =>
+{
+    var enumConverter = new JsonStringEnumConverter(JsonNamingPolicy.CamelCase);
+    opts.JsonSerializerOptions.Converters.Add(enumConverter); 
+});
 
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
